2a255fbba1a1c7caf080d8b991fcecc93e26a721
Calvin,Xu <csxucaixu@gmail.com>
2020-10-14 18:34:35 +0800

    add trees_visualization.

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # xgboost_learning
 
 XGBoost目前在工业界非常常用，所以有必要把XGB的原理和应用系统复习一遍。
-这里整理一些笔记。
+这里整理一些XGBoost学习笔记。
+
+
 
 ## 文件说明
 
-./feature_importance: 特征重要性的说明与应用
+1. feature_importance: 特征重要性的说明与应用
+
+2. trees_visualization: 树可视化分析
 
 
--- trees_visualization/trees_visualization.py
+++ trees_visualization/trees_visualization.py
@@ -10,6 +10,8 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.metrics import roc_auc_score
+from sklearn.feature_selection import SelectFromModel
 
 
 # In[2]:
@@ -50,10 +52,10 @@ xgb_test  = xgb.DMatrix(X_test,  label = Y_test)
 params = {"objective": "binary:logistic",
           "booster": "gbtree",
           "eta": 0.1,
-          "max_depth": 5
+          "max_depth": 2
          }
 
-num_round = 50
+num_round = 20
 
 
 # In[7]:
@@ -65,44 +67,12 @@ watchlist = [(xgb_test, 'eval'), (xgb_train, 'train')]
 # In[8]:
 
 
-bst = xgb.train(params, xgb_train, num_round, watchlist) 
+bst = xgb.train(params, xgb_train, num_round, watchlist)
 
 
 # In[9]:
 
 
-# get_fscore采用默认的weight指标计算特征重要性
-
-bst.get_score(importance_type="weight")
-# bst.get_fscore()
-
-# 降序显示
-# importance = bst.get_score(importance_type="weight")
-# importance = sorted(importance.items(), key = lambda x: x[1], reverse = True)
-# importance
-
-
-# In[10]:
-
-
-# total_gain = gain * weight
-# bst.get_score(importance_type = "gain")
-bst.get_score(importance_type = "total_gain")
-
-
-# In[11]:
-
-
-# bst.get_score(importance_type = "cover")
-bst.get_score(importance_type = "total_cover")
-
-
-# In[12]:
-
-
-# 特征重要性可视化
-import matplotlib.pyplot as plt
-
-xgb.plot_importance(bst, height = 0.5)
-plt.show()
+# xgb单棵树可视化，num_tree共50棵树，取值为 [0, 49]
+xgb.to_graphviz(bst, fmap = '', num_trees = 2)
 
